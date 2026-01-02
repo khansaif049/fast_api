1a2304c6d93330236575ab23172bc15e0ce496e1
saif <saifullah.khan@datenwissen.com>
2026-01-03 01:54:59 +0530

    code added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,HTTPException,Query
+from services.products import get_all_products
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {"message":"Welcome to fastAPi"}
 
-@app.get('product/{id}')
-def get_product_from_id(id:int):
-    products = ['Brush','monitor']
 
-    return products[id]
+
+@app.get("/products")
+def list_products(name:str=Query(default=None,min_length=1,max_length=50,description="Search by Product name (case insensetivity)")):
+    products = get_all_products()
+    if name:
+        needle = name.strip().lower()
+        products = [p for p in products if needle in p.get("name","").lower()]
+        if not products:
+            raise HTTPException(status_code=404,detail=f"No product matching name ={name}")
+        total = len(products)
+
+        return{
+            "total":total,
+            "items":products
+        }
+    return name
+
